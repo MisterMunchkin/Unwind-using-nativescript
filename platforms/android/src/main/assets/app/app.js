4bb0d839d04fc71d7f54def0c39b5471b7041599
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2017-11-18 21:44:56 +0800

    finished addbooking
    bug in json list of load booking

--- platforms/android/src/main/assets/app/app.js
+++ platforms/android/src/main/assets/app/app.js
@@ -1,8 +1,8 @@
 require("./bundle-config");
 const application = require("application");
 
-application.start({ moduleName: "./tabs/tabs-page" });
-//application.start({ moduleName: "Views/login/login" });
+//application.start({ moduleName: "./tabs/tabs-page" });
+application.start({ moduleName: "Views/login/login" });
 /*
 Do not place any code after the application has been started as it will not
 be executed on iOS.
--- platforms/android/src/main/assets/app/tabs/booking/BookingView.js
+++ platforms/android/src/main/assets/app/tabs/booking/BookingView.js
@@ -1,16 +1,57 @@
 const BrowseViewModel = require("./booking-view-model");
 var frameModule = require("ui/frame");
-
+var fetchModule = require("fetch");
 var Observable = require("data/observable").Observable;
 var ObservableArray = require("data/observable-array").ObservableArray;
+
+var items = new ObservableArray([]);
+var pageData = new Observable();
 
 exports.onLoaded = function(args) {
     const component = args.object;
     component.bindingContext = new BrowseViewModel();
+
+    listView(component);
+
+    //
 }
 
 exports.fabTap = function(){
     console.log("shit");
     var topmost = frameModule.topmost();
     topmost.navigate("Views/AddBooking/addbooking");
+}
+
+function listView(component){
+    component.bindingContext = pageData;
+
+    var content = loadBookingData();
+    var limit = Object.keys(content).length;
+
+    /*for(var x = 0;x < limit;x++){
+        items.push(
+            {
+                reservationDate: content["reservationDate"],
+                checkinDate: content["checkinDate"],
+                checkoutDate: content["checkoutDate"],
+                reservationStatus: content["reservationStatus"]
+            }
+        )
+    }
+    
+    pageData.set("items", items);*/
+}
+
+function loadBookingData(){
+    fetchModule.fetch("https://unwindv2.000webhostapp.com/booking/loadBookingData.php", {
+       // method: "POST",
+       // body: formEncode(requestObject)
+    }).then(function (response) {
+
+        console.log(JSON.parse(response._bodyText));
+        //return JSON.parse(response._bodyText);
+
+    }, function (error) {
+        console.log(JSON.stringify(error));
+    })
 }
