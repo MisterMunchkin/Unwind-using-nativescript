fe1ec61078099cadb0576b0cced75dbf71838ae4
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2017-12-04 23:19:41 +0800

    added code for connectivity type
    needs permissions manually added in androidmanifest.xml, wouldn't work so commented it out first

--- platforms/android/src/main/assets/app/Views/login/login.js
+++ platforms/android/src/main/assets/app/Views/login/login.js
@@ -2,7 +2,9 @@ var page;
 var frameModule = require("ui/frame");
 var fetchModule = require("fetch");
 var LoadingIndicator = require("nativescript-loading-indicator-new").LoadingIndicator;
+//var connection = require("tns-core-modules/connectivity");
 
+//var connType = connection.getConnectionType();
 var loader = new LoadingIndicator();
 
 var options = {
@@ -24,6 +26,20 @@ exports.loaded = function(args){ //exports is standard for both nativescript and
 };
 
 exports.signIn = function(){
+    
+   /* switch(connType){
+        case connType.connectionType.none:
+            console.log("no internet connection");
+            break;
+        case connType.connectionType.wifi:
+            console.log("connection: wifi");
+            signInfetch()
+            break;
+        case connType.connectionType.mobile:
+            console.log("connection: mobile");
+            signInfetch()
+            break;  
+    }*/
     var email;
     var password;
 
@@ -31,18 +47,37 @@ exports.signIn = function(){
     password = page.getViewById("password").text;
 
     loader.show(options);
-    var requestObject = {email: email, password: password};
+    var requestObject = { email: email, password: password };
     console.log("attempting to connect to php server");
     fetchModule.fetch("https://unwindv2.000webhostapp.com/login/login.php", {
         method: "POST",
         body: formEncode(requestObject)
-    }).then(function(response){
+    }).then(function (response) {
         then(response);
-    }, function(error){
+    }, function (error) {
         console.log(JSON.stringify(error));
     })
 };
 
+/*function signInfetch(){
+    var email;
+    var password;
+
+    email = page.getViewById("email").text;
+    password = page.getViewById("password").text;
+
+    loader.show(options);
+    var requestObject = { email: email, password: password };
+    console.log("attempting to connect to php server");
+    fetchModule.fetch("https://unwindv2.000webhostapp.com/login/login.php", {
+        method: "POST",
+        body: formEncode(requestObject)
+    }).then(function (response) {
+        then(response);
+    }, function (error) {
+        console.log(JSON.stringify(error));
+    })
+}*/
 function then(response){
     var phpResponse = response._bodyText;
 
