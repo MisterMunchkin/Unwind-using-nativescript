b76011781c927716c14d136566f406d14de4026a
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2017-12-04 23:25:55 +0800

    booking page functionality
    added layout as to the logic within the different functionality inside the booking page

--- app/Views/BookingPage/bookingpage.js
+++ app/Views/BookingPage/bookingpage.js
@@ -2,12 +2,13 @@ var page;
 var frameModule = require("ui/frame");
 var fetchModule = require("fetch");
 
-var pageDataContext = page.navigationContext;
+var pageDataContext;
+var requestObject;
 
 exports.onloaded = function (args) {
     page = args.object
 
-    //var pageDataContext = page.navigationContext;
+    var pageDataContext = page.navigationContext;
 /*
     console.log(pageDataContext.resDate);
     console.log(pageDataContext.checkinDate);
@@ -15,17 +16,32 @@ exports.onloaded = function (args) {
     console.log(pageDataContext.resStatus);
     console.log(pageDataContext.resID);
     */
+    requestObject = {
+        resDate: pageDataContext.resDate,
+        checkinDate: pageDataContext.checkinDate,
+        checkoutDate: pageDataContext.checkoutDate,
+        resStatus: pageDataContext.resStatus,
+        resID: pageDataContext.resID
+    };
+
+    if(requrestObject.resStatus == "Cancelled"){
+        //code for disabling check in button and changing cancel button to an uncancel button
+    }
+
+    //also need code that checks if booking is 24 hours before the check in date, if within the 24 hours then user cannot cancel booking
+
+    
     page.getViewById("resDateLabel").text = pageDataContext.resDate;
 };
 
 exports.cancelButton = function(args){
    // var pageDataContext = page.navigationContext;
 
-    var requestObject = {resDate: pageDataContext.resDate,
+    /*var requestObject = {resDate: pageDataContext.resDate,
                          checkinDate: pageDataContext.checkinDate,
                          checkoutDate: pageDataContext.checkoutDate,
                          resStatus: pageDataContext.resStatus,
-                         resID: pageDataContext.resID};
+                         resID: pageDataContext.resID};*/
     
     console.log(pageDataContext.resDate);
     console.log(pageDataContext.checkinDate);
