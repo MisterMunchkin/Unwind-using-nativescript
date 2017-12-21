96bd226153a948ea4faed22c994b646e2377b45c
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2017-12-21 11:06:19 +0800

    menu optimization
    have to add quantity flow for each item ordered.
    
    probably need a modal for get quantity, or better yet do a master-detail for each food to control

--- app/Views/checkout/checkout_menu.js
+++ app/Views/checkout/checkout_menu.js
@@ -1,18 +1,33 @@
 var page;
 var foodArray;
 var frameModule = require("ui/frame");
+var Observable = require("data/observable").Observable;
+var ObservableArray = require("data/observable-array").ObservableArray;
+
+var items;
+var pageData = new Observable();
+
 
 exports.onLoaded = function(args){
     page = args.object;
-    
-    var pageDataContext = page.navigationContext;
 
+
+    var pageDataContext = page.navigationContext;
+    page.bindingContext = pageData;
+    
     foodArray = pageDataContext.foodArray;
     console.log(JSON.stringify(foodArray));
     var grandTotal = 0;
     var limit = foodArray.length;
     for(var x = 0;x < limit;x++){
-        grandTotal += parseInt(foodArray.price);
+
+        grandTotal += parseInt(foodArray[x].price);
     }
     console.log(grandTotal);
+}
+
+exports.backEvent = function(args){
+    grandTotal = 0;
+    
+    console.log("back pressed");
 }
--- platforms/android/src/main/assets/app/app.js
+++ platforms/android/src/main/assets/app/app.js
@@ -1,6 +1,18 @@
 require("./bundle-config");
 const application = require("application");
 
+var frame = require('ui/frame');
+if (application.android) {
+    application.android.on(application.AndroidApplication.activityBackPressedEvent, backEvent);
+}
+
+function backEvent(args) {
+    var currentPage = frame.topmost().currentPage;
+    if (currentPage && currentPage.exports && typeof currentPage.exports.backEvent === "function") {
+        currentPage.exports.backEvent(args);
+    }
+}
+
 //application.start({ moduleName: "./tabs/tabs-page" });
 application.start({ moduleName: "Views/login/login" });
 /*
