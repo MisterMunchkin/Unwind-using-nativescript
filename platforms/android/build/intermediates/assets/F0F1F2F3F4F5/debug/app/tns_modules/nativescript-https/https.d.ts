b93444e7f4a297cdda9de7f8781c2714ffbe7280
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2018-01-02 13:19:28 +0800

    updates 3.4.0
    updated nativescript, tns-node-modules, and android to 3.4.0

--- platforms/android/build/intermediates/assets/F0F1F2F3F4F5/debug/app/tns_modules/nativescript-https/https.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpResponse } from 'http';
-import * as Https from './https.common';
-export declare function enableSSLPinning(options: Https.HttpsSSLPinningOptions): void;
-export declare function disableSSLPinning(): void;
-export declare function request(opts: Https.HttpsRequestOptions): Promise<HttpResponse>;
-export * from './https.common';
